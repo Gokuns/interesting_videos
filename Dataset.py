a178e0c7c950d2463ee85c3698c044eeb212f386
aslikarahan <aslikarahan96@gmail.com>
2019-10-29 19:31:23 +0300

    added dataset class

--- Dataset.py
+++ Dataset.py
@@ -1,5 +1,4 @@
-import videoData.VideoData as VideoData
-
+from videoData import VideoData
 
 class Dataset:
     max_number_of_people = 0
@@ -18,22 +17,22 @@ class Dataset:
         self.video_path = video_path
         self.videos = videos
 
-        self.min_number_of_people = int(min(video.numberOfPeople for video in videos))
-        self.max_number_of_people = int(max(video.numberOfPeople for video in videos))
-        self.average_number_of_people = sum(video.numberOfPeople for video in videos) / videos.__len__()
+        self.min_number_of_people = int(min(video.number_of_people for video in videos))
+        self.max_number_of_people = int(max(video.number_of_people for video in videos))
+        self.average_number_of_people = sum(video.number_of_people for video in videos) / videos.__len__()
 
-        self.min_number_of_vehicles = int(min(video.numberOfVehicles for video in videos))
-        self.max_number_of_vehicles = int(max(video.numberOfVehicles for video in videos))
-        self.average_number_of_vehicles = sum(video.numberOfVehicles for video in videos) / videos.__len__()
+        self.min_number_of_vehicles = int(min(video.number_of_vehicles for video in videos))
+        self.max_number_of_vehicles = int(max(video.number_of_vehicles for video in videos))
+        self.average_number_of_vehicles = sum(video.number_of_vehicles for video in videos) / videos.__len__()
 
-        self.min_number_of_rare_objects = int(min(video.numberOfRareObjects for video in videos))
-        self.max_number_of_rare_objects = int(max(video.numberOfRareObjects for video in videos))
-        self.average_number_of_rare_objects = sum(video.numberOfRareObjects for video in videos) / videos.__len__()
+        self.min_number_of_rare_objects = int(min(video.number_of_rare_objects for video in videos))
+        self.max_number_of_rare_objects = int(max(video.number_of_rare_objects for video in videos))
+        self.average_number_of_rare_objects = sum(video.number_of_rare_objects for video in videos) / videos.__len__()
 
     def label_videos(self, videos: list):
         for video in videos:
-            video.isInteresting = (video.numberOfRareObjects > self.average_number_of_rare_objects) \
-                                  or ((video.numberOfPeople > self.average_number_of_people)
-                                      and (video.numberOfVehicles > self.average_number_of_vehicles))
+            video.is_interesting = (video.number_of_rare_objects > self.average_number_of_rare_objects) \
+                                  or ((video.number_of_people > self.average_number_of_people)
+                                      and (video.number_of_vehicles > self.average_number_of_vehicles))
 
 
