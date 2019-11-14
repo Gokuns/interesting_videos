2adf47f5bd51801ca4ed14acaca344bdba516ce8
egeonat <egeonat@gmail.com>
2019-11-14 15:19:48 +0300

    Made PCA reduction a function

--- Plotter.py
+++ Plotter.py
@@ -3,14 +3,16 @@ from sklearn.decomposition import PCA
 import numpy as np
 import json
 
-with open("datasets/nuscenes/output_averages_18.json") as f:
-    data = json.load(f)
-    feature_vector_list = [video["features"] for video in data]
-    video_names = [video["video"] for video in data]
-    features = StandardScaler().fit_transform(feature_vector_list)
-    pca = PCA(n_components=20)
-    principal_components = pca.fit_transform(features).tolist()
-    for i in range(len(data)):
-        data[i]["features"] = principal_components[i]
-    with open('data.json', 'w') as outfile:
-        json.dump(data, outfile)
+
+def plot(feature_path):
+    with open(feature_path) as f:
+        data = json.load(f)
+        feature_vector_list = [video["features"] for video in data]
+        features = StandardScaler().fit_transform(feature_vector_list)
+        pca = PCA(n_components=20)
+        principal_components = pca.fit_transform(features).tolist()
+        for i in range(len(data)):
+            data[i]["features"] = principal_components[i]
+        with open('data.json', 'w') as outfile:
+            json.dump(data, outfile)
+        return data
