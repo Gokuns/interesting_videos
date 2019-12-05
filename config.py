9c630aee3160c4d665e0a98f35f4b717def1cdd3
aslikarahan <aslikarahan96@gmail.com>
2019-12-05 22:36:20 +0300

    'config'

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 argument_defaults = dict(
-    dataroot='/Users/Asli/Desktop/train',
-    version='v1.0-mini',
+    dataroot='E:/part1235',
+    version='v1.0-trainval',
     filename='2d_annotations.json',
-    export_path='/Users/Asli/Desktop/lolo',
-    dataset_path='dataset_object.json',
+    export_path='E:/part1235/videos',
+    poc_mode='density_based_both',
+    video_data_path= "C:/Users/Asli/Desktop/poc'ler/dataset_{}.json",
+    feature_path= 'C:\\Users\\Asli\\Desktop\\features\\output_averages_50_peak_number_both.json',
+    colored_graph= True,
+    selected_scene= '',
 )
